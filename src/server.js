ce470fdb6b8dcf09f4c5eabdf0ff26bb2da12edf
Ryan Delaney <rrdelaney@outlook.com>
2016-02-22 20:29:03 -0500

    Using libk for koa stuff

--- src/server.js
+++ src/server.js
@@ -1,40 +1,33 @@
 import Koa from 'koa'
-import route from 'koa-route'
-import bodyParser from 'koa-bodyparser'
+import { bodyParser, route, useState, cors } from 'libk'
 import knex from 'knex'
 import { DB, PORT } from './config'
 
 let db = knex(DB)
 
 console.info('[superfeed] Attempting migration')
-db.migrate.latest()
+db.migrate.latest().then(_ => console.info('[superfeed] Migrations completed'))
 
 let app = new Koa()
 
-app.use((ctx, next) => {
-  ctx.set('Access-Control-Allow-Origin', '*')
-  return next()
-})
+app.use(cors)
+app.use(bodyParser)
+app.use(useState({ db }))
 
-app.use((ctx, next) => {
-  ctx.db = db
-  return next()
-})
-
-app.use(bodyParser())
-
-app.use(route.get('/ping', ctx => {
+app.use(route.GET('/ping')(ctx => {
   ctx.body = 'pong'
 }))
 
-app.use(route.get('/version', async ctx => {
-  let q = ctx.db
+app.use(route.GET('/version')(async ctx => {
+  let q = ctx.state.db
     .select('number')
     .from('version')
     .where('name', 'current')
 
   let [{ number: version }] = await q
+
   ctx.body = { version }
 }))
 
 app.listen(PORT)
+console.info(`[superfeed] App Listening on ${PORT}`)
